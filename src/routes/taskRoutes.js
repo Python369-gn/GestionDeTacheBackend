5c09adbe25dc21872843e8f57a822da6ab79503b
Elhadj Oumar Diallo <elhadjoumarhadiatou1@gmail.com>
2025-08-13 15:53:47 +0000

    Update taskRoutes.js

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth');
+const authorize = require('../middlewares/authorize');
 const {
   getTasks, getTaskById, createTask, updateTask, deleteTask
 } = require('../controllers/taskController');
 
+// Tous les utilisateurs authentifiés peuvent voir et créer des tâches
 router.get('/', auth, getTasks);
 router.post('/', auth, createTask);
+
+// Seul admin ou propriétaire peut voir, modifier ou supprimer une tâche
 router.get('/:id', auth, getTaskById);
 router.put('/:id', auth, updateTask);
 router.delete('/:id', auth, deleteTask);
